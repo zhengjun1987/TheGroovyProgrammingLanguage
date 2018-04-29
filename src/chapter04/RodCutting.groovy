3d06f80a2a9d4d744c05b6fb89ded5ffdab76235
郑军 <zhengjun1987@outlook.com>
2018-04-29 23:38:44 +0800

    《Groovy程序设计》第四章 闭包 2018-04-29 23:38:34

---
+++ src/chapter04/RodCutting.groovy
@@ -0,0 +1,50 @@
+package chapter04
+
+import groovy.transform.Immutable
+
+/**
+ *   Author:Zheng Jun
+ *   E-mail:zhengjun1987@outlook.com
+ *   Date:2018/4/29 22:44
+ *   Project:TheGroovyProgrammingLanguage
+ */
+
+def timeIt(length, closure) {
+    def start = System.nanoTime()
+    println("Max revenue for $length is ${closure(length)}")
+    def end = System.nanoTime()
+    println("It takes ${(end - start) / 1.0e6} milliseconds")
+}
+
+int[] rodPrices = [0, 1, 3, 4, 5, 8, 9, 11, 12, 14, 15, 15, 16, 18, 19, 19, 20, 21, 22, 24, 25, 25, 26, 28, 29, 35, 37, 38, 39, 40]
+def desiredLength = 27
+println "rodPrices[desiredLength] = ${rodPrices[desiredLength]}"   //38
+
+@Immutable
+class RevenueDetails {
+    int revenue
+    ArrayList<Integer> splits
+}
+
+def cutRod
+
+cutRod = {int[] prices, length ->
+    if (length == 0)
+        return new RevenueDetails(revenue: 0, splits: [])
+    else {
+        def maxRevenueDetails = new RevenueDetails(Integer.MIN_VALUE, [] as ArrayList<Integer>)
+        for (rodSize in 1..length) {
+            def revenueFromSecondHalf = cutRod(prices, length - rodSize)
+            def potentialRevenue = new RevenueDetails(prices[rodSize] + revenueFromSecondHalf.revenue, revenueFromSecondHalf.splits + rodSize)
+            if (potentialRevenue.revenue > maxRevenueDetails.revenue)
+                maxRevenueDetails = potentialRevenue
+        }
+        return maxRevenueDetails
+    }
+}.memoize()
+
+timeIt(desiredLength){length -> cutRod(rodPrices,length)}
+//        rodPrices[desiredLength] = 38
+//        Max revenue for 27 is chapter04.RevenueDetails(43, [5, 5, 5, 5, 5, 2])
+//        Disconnected from the target VM, address: '127.0.0.1:57795', transport: 'socket'
+//        It takes 245.43805 milliseconds
--- src/chapter04/TrampolineFactorial.groovy
+++ src/chapter04/TrampolineFactorial.groovy
@@ -10,7 +10,16 @@ factorial = { int number, BigInteger bigInteger -> number == 1 ? bigInteger : fa
 
 println "{factorial(5,1)} = ${factorial(5, 1)}"
 
-println "{factorial(5000,1)} = ${factorial(5000, 1).bitCount()}"
+//println "{factorial(5000,1).bitCount()} = ${factorial(5000, 1).bitCount()}"
 //        {factorial(5,1)} = 120
-//        {factorial(5000,1)} = 24654
+//        {factorial(5000,1).bitCount()} = 24654
 
+static def test(closure){
+    def curry = closure.curry("hello").curry("groovy") //注意此处curry方法返回的是一个新的闭包，而非原来调用curry方法的闭包
+    curry "test"
+}
+//        p1 = hello, p2 = groovy, p3 = test
+
+test() {p1,p2,p3 ->
+    println("p1 = $p1, p2 = $p2, p3 = $p3")
+}
